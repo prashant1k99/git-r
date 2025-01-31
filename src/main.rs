ea0a0ec11add4afa71cad9c70578a0970662b686
Prashant <prashantco111@gmail.com>
2025-01-31 16:08:07 +0530

    Added Clap for CLI and added basic git init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::fs;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Init,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Command::Init => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git directory")
+        }
+    }
 }
